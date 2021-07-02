49f2512296d9341bb26f98a1e992edb37e617ca6
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-07-02 16:55:57 +0530

    feat: Made small string changes

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>
-          { process.env.NEXT_PUBLIC_FAKE_BLOG_NAME } 😉(Original @ <a href={ process.env.NEXT_PUBLIC_ORIGINAL_BLOG_LINK } target="_blank">{ process.env.NEXT_PUBLIC_ORIGINAL_BLOG_NAME }</a>)
+          Super Blog 😉(Original @ <a href="https://blog.greenroots.info" target="_blank">GreenRoots Info</a>)
         </h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -40,7 +40,7 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
       <footer>
-        Feeling something about it? You can contact me on <a href={ process.env.NEXT_PUBLIC_TWITTER_LINK } target="_blank">Twitter</a>.
+        Feeling something about it? You can contact me on <a href="https://twitter.com/tapasadhikary" target="_blank">Twitter</a>.
       </footer>
     </Layout>
   )
